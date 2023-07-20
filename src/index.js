bd650fd3d1685b54c821d2dfdcfaf62846a10398
HerrKallupke <paulgreindl@posteo.de>
2023-07-20 21:22:31 +0200

    Removed HTTPS for usage with reverse proxy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const https_server = require('https')
+const https = require('https')
 const fs = require('fs')
 
 const app = express()
@@ -7,19 +7,14 @@ app.use(express.static('static'))
 app.set('views', 'views/')
 app.set('view engine', 'pug')
 
-const { http, https, key, certificate } = require('../rsc/config.json')
+const { http_port } = require('../rsc/config.json')
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.listen(http, () => {
-    log(`Http server running on port ${http}`)
-})
-
-const options = { key: fs.readFileSync(key), cert: fs.readFileSync(certificate) }
-https_server.createServer(options).listen(https, () => {
-    log(`Https server running on port ${https}`)
+app.listen(http_port, () => {
+    log(`Http server running on port ${http_port}`)
 })
 
 function log(message) {
